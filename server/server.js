418f86aca89875a081599a07e393ab554b403902
Arshan Nawaz <arshannawaz01@gmail.com>
2024-10-06 21:21:00 +0500

    Refactor server.js and env.config.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import connectDB from './src/config/db.config.js';  // Adjust the path as needed
 import { app } from './app.js';
+import { PORT } from './src/config/env.config.js';
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(PORT, () => {
       // eslint-disable-next-line
-      console.log(`⚙️ Server is running on port ${process.env.PORT}`);
+      console.log(`⚙️ Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
